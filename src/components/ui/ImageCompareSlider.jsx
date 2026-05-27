6a1ccb8342da5ac608d54b3672066245aecbe7b3
p1umpe <borschalex24@gmail.com>
2026-05-27 13:17:16 +0500

    тема для слайдера

--- src/components/ui/ImageCompareSlider.jsx
+++ src/components/ui/ImageCompareSlider.jsx
@@ -100,7 +100,7 @@ export const ImageCompareSlider = ({
         >
           <div className="image-compare-slider-line" />
           <div className="image-compare-slider-handle">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
               <path
                 d="M9 18L15 12L9 6"
                 stroke="currentColor"
